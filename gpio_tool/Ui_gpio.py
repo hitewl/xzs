12f849d7ad5ae3a14f1e57ea408493c772f1327e
hitewl <1599247708@qq.com>
2024-12-07 17:21:56 +0800

    递交新的gpio_tool

---
+++ gpio_tool/Ui_gpio.py
@@ -0,0 +1,162 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'gpio.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QHBoxLayout, QLabel,
+    QLineEdit, QMainWindow, QMenu, QMenuBar,
+    QPushButton, QSizePolicy, QStatusBar, QTextBrowser,
+    QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(288, 394)
+        self.actionopen = QAction(MainWindow)
+        self.actionopen.setObjectName(u"actionopen")
+        icon = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.FolderNew))
+        self.actionopen.setIcon(icon)
+        self.actionsave = QAction(MainWindow)
+        self.actionsave.setObjectName(u"actionsave")
+        icon1 = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.DocumentSave))
+        self.actionsave.setIcon(icon1)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_3 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.textshowinfo = QTextBrowser(self.centralwidget)
+        self.textshowinfo.setObjectName(u"textshowinfo")
+
+        self.verticalLayout_2.addWidget(self.textshowinfo)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.horizontalLayout_2 = QHBoxLayout()
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.label_2 = QLabel(self.centralwidget)
+        self.label_2.setObjectName(u"label_2")
+        font = QFont()
+        font.setFamilies([u"DengXian"])
+        font.setPointSize(12)
+        self.label_2.setFont(font)
+
+        self.horizontalLayout_2.addWidget(self.label_2)
+
+        self.clear_button = QPushButton(self.centralwidget)
+        self.clear_button.setObjectName(u"clear_button")
+
+        self.horizontalLayout_2.addWidget(self.clear_button)
+
+        self.combox = QComboBox(self.centralwidget)
+        self.combox.addItem("")
+        self.combox.addItem("")
+        self.combox.addItem("")
+        self.combox.setObjectName(u"combox")
+
+        self.horizontalLayout_2.addWidget(self.combox)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout_2)
+
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        font1 = QFont()
+        font1.setFamilies([u"Arial"])
+        font1.setPointSize(12)
+        self.label.setFont(font1)
+
+        self.horizontalLayout.addWidget(self.label)
+
+        self.lineEdit = QLineEdit(self.centralwidget)
+        self.lineEdit.setObjectName(u"lineEdit")
+
+        self.horizontalLayout.addWidget(self.lineEdit)
+
+
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+
+        self.verticalLayout_2.addLayout(self.verticalLayout)
+
+        self.button_search = QPushButton(self.centralwidget)
+        self.button_search.setObjectName(u"button_search")
+        font2 = QFont()
+        font2.setFamilies([u"\u5b8b\u4f53"])
+        font2.setPointSize(14)
+        self.button_search.setFont(font2)
+        icon2 = QIcon(QIcon.fromTheme(QIcon.ThemeIcon.EditFind))
+        self.button_search.setIcon(icon2)
+
+        self.verticalLayout_2.addWidget(self.button_search)
+
+
+        self.verticalLayout_3.addLayout(self.verticalLayout_2)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 288, 33))
+        self.menu = QMenu(self.menubar)
+        self.menu.setObjectName(u"menu")
+        self.menu_2 = QMenu(self.menubar)
+        self.menu_2.setObjectName(u"menu_2")
+        self.menu_3 = QMenu(self.menubar)
+        self.menu_3.setObjectName(u"menu_3")
+        self.menu_4 = QMenu(self.menubar)
+        self.menu_4.setObjectName(u"menu_4")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menu.menuAction())
+        self.menubar.addAction(self.menu_2.menuAction())
+        self.menubar.addAction(self.menu_3.menuAction())
+        self.menubar.addAction(self.menu_4.menuAction())
+        self.menu.addAction(self.actionopen)
+        self.menu.addAction(self.actionsave)
+
+        self.retranslateUi(MainWindow)
+
+        self.combox.setCurrentIndex(0)
+
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.actionopen.setText(QCoreApplication.translate("MainWindow", u"\u6253\u5f00", None))
+        self.actionsave.setText(QCoreApplication.translate("MainWindow", u"\u4fdd\u5b58", None))
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\u5e73\u53f0", None))
+        self.clear_button.setText(QCoreApplication.translate("MainWindow", u"clear", None))
+        self.combox.setItemText(0, QCoreApplication.translate("MainWindow", u"N10", None))
+        self.combox.setItemText(1, QCoreApplication.translate("MainWindow", u"N8", None))
+        self.combox.setItemText(2, QCoreApplication.translate("MainWindow", u"N4", None))
+
+        self.label.setText(QCoreApplication.translate("MainWindow", u"gpio", None))
+        self.button_search.setText(QCoreApplication.translate("MainWindow", u"\u67e5\u8be2", None))
+        self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\u6587\u4ef6", None))
+        self.menu_2.setTitle(QCoreApplication.translate("MainWindow", u"\u7f16\u8f91", None))
+        self.menu_3.setTitle(QCoreApplication.translate("MainWindow", u"\u5e2e\u52a9", None))
+        self.menu_4.setTitle(QCoreApplication.translate("MainWindow", u"\u9000\u51fa", None))
+    # retranslateUi
+
---
+++ gpio_tool/gpio_data.py
@@ -0,0 +1,15 @@
+import os
+import json
+class plat_config:
+    config_data = None
+    select_plat = "N10"
+    def __init__(self):
+        self.current_file_path = os.path.dirname(os.sys.argv[0])
+        self.gpio_rev_file_path = "config\gpio_resource.json5"
+        self.gpio_config_data_plat = os.path.join(self.current_file_path, self.gpio_rev_file_path )
+        print(self.gpio_config_data_plat)
+        with open(self.gpio_config_data_plat, 'r') as fconfig:
+           self.config_data = json.load(fconfig)
+      
+if __name__  == '__main__':
+    plat = plat_config()
